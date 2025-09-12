c6ba0fa040fa68a1d2f321a67667316e230ac05d
vhatkarsumit74 <sumitvhatkar74@gmail.com>
2025-09-12 23:09:14 +0530

    Project Initialization

---
+++ src/main/java/com/aura/connectcampus/ConnectcampusApplication.java
@@ -0,0 +1,13 @@
+package com.aura.connectcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectcampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectcampusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aura/connectcampus/ConnectcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aura.connectcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectcampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
